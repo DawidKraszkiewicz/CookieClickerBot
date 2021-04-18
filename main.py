5cdb705408e8fa3396dbaf322e9be1f5dd8a6c4e
DawidKraszkiewicz <39964989+DawidKraszkiewicz@users.noreply.github.com>
2021-04-18 20:52:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-chromedriver_path =r"C:\Users\krasz\OneDrive\Desktop\Bot\chromedriver.exe"
+chromedriver_path =r"your chromedriver path"
 driver = webdriver.Chrome(executable_path=chromedriver_path)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element_by_css_selector("#cookie")
@@ -31,27 +31,20 @@ while True:
         if "," in money_element:
             money_element = money_element.replace(",", "")
         cookie_count = int(money_element)
-
-        # Find upgrades that we can currently afford
+        
         affordable_upgrades = {}
         for cost, id in cookie_upgrades.items():
             if cookie_count > cost:
                 affordable_upgrades[cost] = id
 
-        # Purchase the most expensive affordable upgrade
         highest_price_affordable_upgrade = max(affordable_upgrades)
         to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
 
         driver.find_element_by_id(to_purchase_id).click()
 
-        # Add another 5 seconds until the next check
         timeout = time.time() + 5
 
-        # After 5 minutes stop the bot and check the cookies per second count.
-    if time.time() > five_min:
-        cookie_per_s = driver.find_element_by_id("cps").text
-        print(cookie_per_s)
-        break
+        
 
 
 
